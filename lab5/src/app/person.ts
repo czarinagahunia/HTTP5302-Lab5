917a41a4283d8d04c3b12b3518ee010ac7fab7f8
Czarina <czarinagahunia@gmail.com>
2018-06-08 23:01:45 -0400

    Part 1: Person Model

---
+++ lab5/src/app/person.ts
@@ -0,0 +1,28 @@
+export class Person {
+    id : number;
+    firstName : string;
+    lastName : string;
+    dateOfBirth : Date;
+
+    // This method will return an accurate age based on the dateOfBirth
+    getAge() : number {
+
+        // New instance of a new Date object
+        let today = new Date();
+
+        // Subtract dateOfBirth year from the present year to get the age
+        let age = today.getFullYear() - this.dateOfBirth.getFullYear();
+
+        // If dateOfBirth month and date are greater than today's, deduct age by 1 to get accurate result 
+        if (
+            this.dateOfBirth.getDate() > today.getDate() 
+            && 
+            this.dateOfBirth.getMonth() > today.getMonth()
+        ) {
+            return age--;
+        }
+
+        return age;
+
+    }
+}
